8b9decef287df28e96b61df8678a33ebe4d81437
Jeon Sehoon <jeonsehoon@macbook-pro-2.local>
2018-05-15 15:45:14 +0900

    Initial Commit

---
+++ SampleSourceCode/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SampleSourceCode
+//
+//  Created by Jeon Sehoon on 2018. 5. 15..
+//  Copyright © 2018년 Jeon Sehoon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
